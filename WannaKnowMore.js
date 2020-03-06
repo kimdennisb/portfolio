6a6bf28ab4b9adf03011663539336984cb91f8ca
Dennis Kim <kimutaidennisbrian@gmail.com>
2020-03-06 18:23:00 +0300

    Initial commit

---
+++ WannaKnowMore.js
@@ -0,0 +1,8 @@
+
+//getting elements and animating them.
+    document.getElementById('knowmore').addEventListener("click",function(){
+    document.getElementById('formore').style.display="block"
+    });
+    document.getElementById('knowmore').addEventListener("click",function(){
+    document.getElementById('knowmore').style.display="none"
+    });
